68a81b2f36b90ee21f6e64450cf38887a8de71f7
safadigital <safarov@safadigital.su>
2025-05-20 10:35:25 +0500

    added code form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 const LoginPage = () => {
 
     const [email, setEmail] = useState<string>('');
+    const [isEmailSent, setIsEmailSent] = useState<boolean>(false);
+
     const [code, setCode] = useState<string>('');
 
     return (
@@ -10,10 +12,20 @@ const LoginPage = () => {
         <div className="main_container">
 
 {
-    email.length < 1 && (
+    isEmailSent != true && (
             <form className="w-100 h-100 flex flex-col gap-3 justify-center items-center">
                 <input className="border-blue-500 w-50 p-2" type="email" placeholder="johndoe@example.com" required />
-                <button disabled={!email} className="bg-blue-500 text-white w-50 p-2">Submit</button>
+                <button disabled={!email} className="bg-blue-500 text-white w-50 p-2 cursor-pointer">Submit</button>
+            </form>
+
+    )
+}
+
+{
+    isEmailSent === true && (
+            <form className="w-100 h-100 flex flex-col gap-3 justify-center items-center">
+                <input className="border-blue-500 w-50 p-2" type="text" placeholder="12345" required />
+                <button disabled={code.length < 6} className="bg-blue-500 text-white w-50 p-2 cursor-pointer">Send the code</button>
             </form>
 
     )
