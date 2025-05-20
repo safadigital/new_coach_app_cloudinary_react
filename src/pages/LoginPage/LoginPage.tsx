20b1df2151aefe84d743c7b438fa48b2c1cda676
safadigital <safarov@safadigital.su>
2025-05-20 10:25:37 +0500

    added the login UI

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 const LoginPage = () => {
 
+    const [email, setEmail] = useState<string>('');
+    const [code, setCode] = useState<string>('');
+
     return (
-        <h1>Login page</h1>
+        <>
+        <div className="main_container">
+
+{
+    email.length < 1 && (
+            <form className="w-100 h-100 flex flex-col gap-3 justify-center items-center">
+                <input className="border-blue-500 w-50 p-2" type="email" placeholder="johndoe@example.com" required />
+                <button disabled={!email} className="bg-blue-500 text-white w-50 p-2">Submit</button>
+            </form>
+
+    )
+}
+          
+
+        </div>
+        </>
     )
 }
 
