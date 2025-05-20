b94a9862d66edfac21902fa8516f00efc36f3de5
safadigital <safarov@safadigital.su>
2025-05-20 12:44:20 +0500

    added func for login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const LoginPage = () => {
 
+    const navigate = useNavigate();
+
     const [isLoading, setIsLoading] = useState<boolean>(false);
+
     const [email, setEmail] = useState<string>('');
     const [isEmailSent, setIsEmailSent] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
 
     const [code, setCode] = useState<string>('');
 
@@ -12,7 +17,7 @@ const LoginPage = () => {
     const emailHandler = (e: any) => {
         e.preventDefault();
         setIsLoading(true);
-        axios.post(`http://content-dev.the.coach/api/v1/user/web/verify_email/`,
+        axios.post(import.meta.env.VITE_API_VERIFY_EMAIL_URL,
             {
                 "email": email
             }
@@ -28,9 +33,31 @@ const LoginPage = () => {
             setIsLoading(false);
         }).catch(error => {
     console.error('Error for email sending handler: ', error);
+    setError(error);
 });
 
     }
+
+
+const codeHandler = (e: any) => {
+        e.preventDefault();
+        setIsLoading(true);
+        const base_token_url = import.meta.env.VITE_API_VERIFY_TOKEN_CODE_BASE_URL;
+        axios.get(base_token_url + `?code=${code}&email=${email}`, {
+             headers: {
+                'Authorization': '',
+                
+            }
+        }).then((response: any) => {
+              console.log('RESPONSE FROM SERVER FROM CODE:', response);
+              setEmail('');
+              setCode('');
+              navigate('/');
+        }).catch(error => {
+    console.error('Error for code sending handler: ', error);
+    setError(error);
+}); 
+}
 
     if (isLoading === true) {
         return (
@@ -55,6 +82,11 @@ const LoginPage = () => {
                 onChange={(e: any) => setEmail(e.target.value) }
                 className="border-blue-500 w-50 p-2" type="email" placeholder="johndoe@example.com" required />
                 <button disabled={!email} className="bg-blue-500 text-white w-50 p-2 cursor-pointer">Submit</button>
+                {
+                    !!error && (
+                        <p className="text-red-600">Error occured!</p>
+                    )
+                }
             </form>
 
     )
@@ -62,11 +94,18 @@ const LoginPage = () => {
 
 {
     isEmailSent === true && (
-            <form className="w-100 h-100 flex flex-col gap-3 justify-center items-center">
+            <form
+            onSubmit={codeHandler}
+            className="w-100 h-100 flex flex-col gap-3 justify-center items-center">
                 <input
                  onChange={(e: any) => setCode(e.target.value) }
                 className="border-blue-500 w-50 p-2" type="text" placeholder="12345" required />
                 <button disabled={code.length < 6} className="bg-blue-500 text-white w-50 p-2 cursor-pointer">Send the code</button>
+                 {
+                    !!error && (
+                        <p className="text-red-600">Error occured!</p>
+                    )
+                }
             </form>
 
     )
